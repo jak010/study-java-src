74ec4ec2122fe39c9006f93a5e7576782a940f8a
jak010 <bluetoon@naver.com>
2022-12-12 00:14:08 +0900

    add, array part

---
+++ Chapter05/ArrayEx1.java
@@ -0,0 +1,25 @@
+package Chapter05;
+
+import java.util.Arrays;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+
+        int[] score = new int[5];
+        int k = 1;
+
+        for (int i = 1; i < 5; i++) {
+            score[i] = i * 10;
+        }
+
+        for (int sc : score) {
+            System.out.println(sc);
+        }
+
+        int tmp = score[k + 2] + score[4];
+
+        System.out.println(Arrays.toString(score));
+
+
+    }
+}
---
+++ Chapter05/Notepad.java
@@ -0,0 +1,14 @@
+package Chapter05;
+
+import java.util.Arrays;
+
+public class Notepad {
+    public static void main(String[] args) {
+        char[] abc = {'A', 'B', 'C', 'D'};
+        char[] result = new char[abc.length];
+
+        System.arraycopy(abc, 0, result, 0, abc.length);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
