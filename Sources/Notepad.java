19bac391bc312dc88e1f0496d80b9ed578c6e686
jak010 <bluetoon@naver.com>
2025-08-05 23:42:54 +0900

    feat: Notepad.java: 숫자 포맷 적용 예제 추가

--- Sources/Notepad.java
+++ Sources/Notepad.java
@@ -3,35 +3,20 @@ package Sources;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.text.DecimalFormat;
+
 public class Notepad {
 
 
     public static void main(String[] args) {
 
 
-        // blank String
-        String blankString = "";
-        if (StringUtils.isBlank(blankString)) {
-            System.out.println("T");
-        } else {
-            System.out.println("F");
-        }
+        DecimalFormat formatter = new DecimalFormat("#,##0");
 
-        System.out.println("========================");
+        int data = 1000000;
+        String ret = formatter.format(data);
 
-        // Trim String
-        String trimStirng = " a ";
-        System.out.println(trimStirng);
-        System.out.println(StringUtils.trim(trimStirng));
-
-        //  Compare String
-        String compareString = "notnull";
-        System.out.println(StringUtils.compare(compareString, "notnull"));
-
-        // Equals ?
-        String compareString2 = "notnull";
-        System.out.println(StringUtils.equals(compareString2, "notnull")); // true
-        System.out.println(StringUtils.equals(compareString2, "notnul2")); // false
+        System.out.println(ret);
 
     }
 
