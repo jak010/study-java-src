977e7da5cbdd6d1ce2a3b6d0cda2395103eb52be
jak010 <bluetoon@naver.com>
2025-09-16 09:35:58 +0900

    fix(BOJ1110): 주석 추가 및 코드 정리

--- algorithm/backjoon/BOJ1110.java
+++ algorithm/backjoon/BOJ1110.java
@@ -19,6 +19,7 @@ public class BOJ1110 {
         while (true) {
             int first, second = 0;  // 문제에서는 두 자리수 연산이기 때문에 10의 자리, 1의 자리로 다룰 수를 지정함
 
+            // 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다.
             if (next < 10) {
                 first = 0;
                 second = next;
@@ -30,6 +31,9 @@ public class BOJ1110 {
                 second = next % 10;
             }
             int sum = first + second;
+            // (주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면)
+            // next%10 * 10 : 10의 자리수
+            // sum %10 : 1의 자리수
             next = (next % 10 * 10) + (sum % 10);
 
             if (ori == next) {
